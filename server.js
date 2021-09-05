df9bd499a1c9f7af8c92a863e03aa5c0e8292eb1
ksingh_itp <ksingh.itp@sparkcognition.com>
2021-09-05 11:52:32 +0530

    socket message done

--- server.js
+++ server.js
@@ -27,20 +27,51 @@ app.use('/channel', channelRoutes)
 app.use('/message', messageRoutes);
 
 
+var users = [];
+
+function addUserInList(user, socketId) {
+    var ele = users.find(u => u.id == user.userId);
+    if (ele) return;
+    users.push({
+        id: user.userId,
+        name: user.name,
+        socketId: socketId
+    });
+}
+
+
+function removeUserFromList(socketId) {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+
 io.on('connection', socket => {
 
     console.log('A new user is connected')
+
+    socket.on('addUser', user => {
+        addUserInList(user, socket.id);
+        io.emit('getUsers', users);
+    });
 
     socket.on('join', ({ name }, callback) => {
         users = [...users, name];
         callback(users);
     })
 
+
+    socket.on('newMessage', data => {
+        console.log(data)
+        io.to(data.receiverSocketId).emit('message', data);
+    });
+
     socket.on('disconnect', () => {
+        removeUserFromList(socket.id)
+        io.emit('getUsers', users);
         console.log('User had disconnected')
 
     });
-    console.log(socket);
+
 });
 
 
